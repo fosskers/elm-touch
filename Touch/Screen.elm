df55068bf653bc7853621daad83c5e742c35978f
Colin Woodbury <colingw@gmail.com>
2014-04-15 10:02:01 -0700

    `angle` function works
    - It is based off the unit circle (with quadrants being where they should)
      and yields angles between (tau / 2) and (-tau / 2).

--- Touch/Screen.elm
+++ Touch/Screen.elm
@@ -2,22 +2,43 @@ module Touch.Screen where
 
 import Touch
 
+-- Only for testing
+import Mouse as M
+import Window as W
+
 ---
 
 data Cardinal = Up | UpRight | Right | DownRight | Down | DownLeft | Left | UpLeft
 
+data Swipe = Swipe SwipeType Cardinal
+
+data SwipeType = OneFinger | TwoFinger | ThreeFinger
+
 {-
 -- | Yields a Cardinal relative to a given fixed location.
 relative : (Int,Int) -> Signal Cardinal
+-- Should be based off `taps` and `angle`
 
 -- | Yields a Cardinal based off the angle of a swiping Touch.
 cardinal : Signal Cardinal
 -- Should be based on `swipe`.
 
-swipe : Signal (Radians angle representation?)
+swipe : Signal Float (Radians angle representation?)
 -- Based on `touches`.
 -- Can there not just be `touch` that yields the latest Touch, like
 -- Keyboard.arrows does?
 -- Note that `taps` also exists (shouldn't it be `tap`?) meaning that single
 -- (non-list) outputs are possible.
 -}
+
+-- | Calculates the angle between two points on the screen.
+-- Based on the standard unit circle. Angles range from
+-- pi to -pi.
+angle : (Int,Int) -> (Int,Int) -> Float
+angle (x1,y1) (x2,y2) = atan2 (toFloat (y2 - y1)) (toFloat (x1 - x2))
+
+render : (Int,Int) -> (Int,Int) -> Element
+render mouse center = asText <| angle mouse center
+
+main : Signal Element
+main = render <~ M.position ~ ((\(x,y) -> (x `div` 2, y `div` 2)) <~ W.dimensions)
